65112333cff7240297d72c142bb5f46c2db04105
francis <jesusloves.francis@gmail.com>
2015-08-18 14:43:24 +0530

    fix bugs

--- console/migrations/m141121_104616_project_table.php
+++ console/migrations/m141121_104616_project_table.php
@@ -3,21 +3,12 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m141121_104616_project_table extends Migration
+class m141121_104616_update_user_table extends Migration
 {
     public function up()
     {
         $this->execute(
-            "CREATE TABLE project (
-              id int(11) NOT NULL AUTO_INCREMENT,
-              project_name varchar(50) NOT NULL,
-              username varchar(50) NOT NULL,
-              company varchar(50) NOT NULL,
-              created_at int(11) NOT NULL,
-              updated_at int(11) NOT NULL,
-              priority enum('high','normal','low') NOT NULL,
-              PRIMARY KEY (id)
-            ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;"
+            "ALTER TABLE user ADD first_name VARCHAR(25) NULL AFTER id, ADD last_name VARCHAR(25) NULL AFTER first_name;"
         );
     }
 
